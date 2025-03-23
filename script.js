95eca16459e4a2912d4b1368890945e8561824a8
Yvonne-del <ys359375@gmail.com>
2025-03-24 01:18:47 +0300

    clean code

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     const movieDisplay = document.getElementById("movie-display");
     const movieInfo = document.getElementById("movie-information");
 
+    //GET the movie titles and display them in browser
     async function fetchMovies() {
         const res = await fetch(baseURL);
         const movies = await res.json();
@@ -23,15 +24,18 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
     }
 
+    //GET movie details when movie title is clicked
     async function getMovieDetails(movieId) {
         const res = await fetch(`${baseURL}/${movieId}`);
         const movie = await res.json();
 
+        //ensure to load the capacity and ticket_sold values correctly as numbers
         const capacity = Number(movie.capacity) || 0;
         const ticketsSold = Number(movie.tickets_sold) || 0;
         let remainingTickets = capacity - ticketsSold;
-
+        //display movie poster in the first div element
         movieDisplay.innerHTML = `<img src="${movie.poster}" alt="${movie.title}" width="200">`;
+        //display movie details in the third div element
         movieInfo.innerHTML = `
             <h2>${movie.title}</h2>
             <p><strong>Runtime: </strong>${movie.runtime}</p>
@@ -40,20 +44,18 @@ document.addEventListener("DOMContentLoaded", async function () {
             <p><strong>Tickets Remaining:</strong> <span id="ticketCount">${remainingTickets}</span></p>
             <button id="ticketButton" type="button" ${remainingTickets === 0 ? "disabled" : ""}>Buy Ticket</button>
         `;
-
+        //add event listener to the ticket button to make changes after buying a ticket
         const ticketButton = document.getElementById("ticketButton");
-        ticketButton.addEventListener("click", (event) => {
-            event.preventDefault();
-            purchaseTicket(movie, remainingTickets);
-        });
+        ticketButton.addEventListener("click", event => purchaseTicket(movie, remainingTickets));
     }
 
+    //prevent buying of tickets when tickets end
     function purchaseTicket(movie, remainingTickets) {
         if (remainingTickets > 0) {
             remainingTickets--;
             document.getElementById("ticketCount").textContent = remainingTickets;
         }
     }
-
+    
     fetchMovies();
 });
