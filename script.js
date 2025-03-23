c38936ce925fface8edcfabae6a8e03403d410b1
Yvonne-del <ys359375@gmail.com>
2025-03-23 20:39:10 +0300

    add event listener to fetch details of movie after clicking movie title

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const baseURL = "http://localhost:3000/films"
 const movieList = document.getElementById("movieList-container")
+const movieDisplay = document.getElementById("movie-display")
+const movieInfo = document.getElementById("movie-information")
 //start by creating the movie list
 //GET movie titles and display them in the first div element
 async function getMovieList(){
@@ -11,7 +13,11 @@ async function getMovieList(){
     data.forEach(item => {
         const movies = document.createElement("p")
         movies.textContent = `${item.title.toUpperCase()}`
+        movieItem.dataset.movieId = item.id; // Store movie ID for fetching details later
         movieList.appendChild(movies);
+        //to fetch use movie details for display 
+        movieItem.addEventListener("click", () => getMovieDetails(item.id));
     });
 }
 getMovieList();
+movie-information
